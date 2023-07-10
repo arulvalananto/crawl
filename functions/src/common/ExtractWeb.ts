7619cc5c309543da9b9a58efeb43ebb557a24ad0
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2023-07-10 21:49:05 +0530

    fix: eslint issues

--- functions/src/common/ExtractWeb.ts
+++ functions/src/common/ExtractWeb.ts
@@ -1,143 +1,212 @@
-import * as cheerio from 'cheerio';
+import * as cheerio from "cheerio";
 
-import constants from './constants';
-import { estimateReadingTime, extractKeywords } from './helpers';
+import constants from "./constants";
+import {estimateReadingTime, extractKeywords} from "./helpers";
 
+/**
+ * Extract webpage data
+ */
 class ExtractWeb {
-    private url: string;
-    private $: cheerio.CheerioAPI;
+  private url: string;
+  private $: cheerio.CheerioAPI;
 
-    constructor(url: string, html: string) {
-        this.url = url;
-        this.$ = cheerio.load(html);
-    }
+  /**
+    * Constructor function for ExtractWeb
+    * @param {string} url
+    * @param {string} html
+    */
+  constructor(url: string, html: string) {
+    this.url = url;
+    this.$ = cheerio.load(html);
+  }
 
-    private getUrl() {
-        return this.url;
-    }
+  /**
+    * Get website url
+    * @return {string} url
+    */
+  private getUrl() {
+    return this.url;
+  }
 
-    private meta(
-        value: string,
-        name = constants.PROPERTY,
-        attr = constants.CONTENT
-    ) {
-        return this.$(`meta[${name}="${value}"]`).attr(attr);
-    }
+  /**
+    * Get information from Meta
+    * @param {string} value
+    * @param {string} name
+    * @param {string} attr
+    * @return {string} content
+    */
+  private meta(
+    value: string,
+    name = constants.PROPERTY,
+    attr = constants.CONTENT
+  ) {
+    return this.$(`meta[${name}="${value}"]`).attr(attr);
+  }
 
-    private getTitle() {
-        return this.$(constants.TITLE).text() || constants.UNTITLED;
-    }
+  /**
+    * Get title information from webpage
+    * @return {string} title
+    */
+  private getTitle() {
+    return this.$(constants.TITLE).text() || constants.UNTITLED;
+  }
 
-    private getThumbnail() {
-        return this.meta(constants.OG_IMAGE);
-    }
+  /**
+    * Get thumbnail information from webpage
+    * @return {string} thumbnail
+    */
+  private getThumbnail() {
+    return this.meta(constants.OG_IMAGE);
+  }
 
-    private getSiteName() {
-        return (
-            this.meta(constants.OG_SITE_NAME) ||
+  /**
+    * Extract site name form url
+    * @return {string} site_name
+    */
+  private getSiteName() {
+    return (
+      this.meta(constants.OG_SITE_NAME) ||
             this.meta(constants.NAME, constants.ITEM_PROP) ||
             this.meta(constants.TWITTER_SITE, constants.NAME)
-        );
-    }
+    );
+  }
 
-    private getDescription() {
-        return (
-            this.$(constants.META_DESCRIPTION).text() ||
+  /**
+    * Extract description from url
+    * @return {string} description
+    */
+  private getDescription() {
+    return (
+      this.$(constants.META_DESCRIPTION).text() ||
             this.meta(constants.OG_DESCRIPTION) ||
             this.meta(constants.TWITTER_DESCRIPTION) ||
-            ''
-        );
+            ""
+    );
+  }
+
+  /**
+    * Get type of website from url
+    * @return {string} type
+    */
+  private getType() {
+    return this.meta(constants.OG_TYPE) || "";
+  }
+
+  /**
+    * Get keywords from url content / meta tag
+    * @return {string[]} keywords
+    */
+  private getKeywords() {
+    const keywords = this.meta(constants.KEYWORDS) || [];
+
+    if (keywords.length) return keywords;
+    else {
+      const title = this.getTitle();
+      const description = this.getDescription();
+
+      const textContent = this.$(constants.HEADER_SELECTOR).text();
+      const combineContent = `${title} ${description} ${textContent}`;
+      return extractKeywords(combineContent);
     }
+  }
 
-    private getType() {
-        return this.meta(constants.OG_TYPE) || '';
-    }
+  /**
+    * Get website logo
+    * @return {string} logo
+    */
+  private getLogo() {
+    return this.$(constants.IMAGE_WITH_LOGO).attr(constants.SOURCE);
+  }
 
-    private getKeywords() {
-        const keywords = this.meta(constants.KEYWORDS) || [];
-
-        if (keywords.length) return keywords;
-        else {
-            const title = this.getTitle();
-            const description = this.getDescription();
-
-            const textContent = this.$(constants.HEADER_SELECTOR).text();
-            const combineContent = `${title} ${description} ${textContent}`;
-            return extractKeywords(combineContent);
-        }
-    }
-
-    private getLogo() {
-        return this.$(constants.IMAGE_WITH_LOGO).attr(constants.SOURCE);
-    }
-
-    private getAuthor() {
-        return (
-            this.meta(constants.OG_AUTHOR) ||
+  /**
+    * Extract author name
+    * @return {string} author
+    */
+  private getAuthor() {
+    return (
+      this.meta(constants.OG_AUTHOR) ||
             this.meta(constants.AUTHOR, constants.NAME)
-        );
+    );
+  }
+
+  /**
+    * Extractor for icons
+    * @param {string} name
+    * @param {string} type
+    * @return {any[]} icons[]
+    */
+  private getIconExtractor(name: string, type: string) {
+    const arr: any[] = [];
+
+    this.$(name).each((_, element) => {
+      const href = this.$(element).attr(constants.HREF);
+      const sizes = this.$(element).attr(constants.SIZES);
+
+      arr.push({
+        type,
+        sizes: sizes ? sizes : constants.DEFAULT_ICON_SIZE,
+        href,
+      });
+    });
+
+    return arr;
+  }
+
+  /**
+    * Extract icons from url
+    * @return {any[]} icons
+    */
+  private getIcons() {
+    const icons = this.getIconExtractor(
+      constants.ICON_SELECTOR,
+      constants.ICON
+    );
+    const appleIcons = this.getIconExtractor(
+      constants.APPLE_ICON_SELECTOR,
+      constants.APPLE_TOUCH_ICON
+    );
+    const maskIcons = this.getIconExtractor(
+      constants.MASK_ICON_SELECTOR,
+      constants.MASK_ICON
+    );
+
+    return [...icons, ...appleIcons, ...maskIcons];
+  }
+
+  /**
+    * Measure reading time for the website if it is an article.
+    * @return {number} reading_time
+    */
+  private getReadingTime() {
+    const type = this.meta(constants.OG_TYPE);
+    if (type === constants.ARTICLE) {
+      const content = this.$(constants.HEADER_WITH_PARAGRAPH).text();
+
+      const readingTime = estimateReadingTime(content);
+      return readingTime;
     }
+    return undefined;
+  }
 
-    private getIconExtractor(name: string, type: string) {
-        const arr: any[] = [];
-
-        this.$(name).each((_, element) => {
-            const href = this.$(element).attr(constants.HREF);
-            const sizes = this.$(element).attr(constants.SIZES);
-
-            arr.push({
-                type,
-                sizes: sizes ? sizes : constants.DEFAULT_ICON_SIZE,
-                href,
-            });
-        });
-
-        return arr;
-    }
-
-    private getIcons() {
-        const icons = this.getIconExtractor(
-            constants.ICON_SELECTOR,
-            constants.ICON
-        );
-        const appleIcons = this.getIconExtractor(
-            constants.APPLE_ICON_SELECTOR,
-            constants.APPLE_TOUCH_ICON
-        );
-        const maskIcons = this.getIconExtractor(
-            constants.MASK_ICON_SELECTOR,
-            constants.MASK_ICON
-        );
-
-        return [...icons, ...appleIcons, ...maskIcons];
-    }
-
-    private getReadingTime() {
-        const type = this.meta(constants.OG_TYPE);
-        if (type === constants.ARTICLE) {
-            const content = this.$(constants.HEADER_WITH_PARAGRAPH).text();
-
-            const readingTime = estimateReadingTime(content);
-            return readingTime;
-        }
-        return undefined;
-    }
-
-    getData() {
-        return {
-            url: this.getUrl(),
-            type: this.getType(),
-            logo: this.getLogo(),
-            title: this.getTitle(),
-            author: this.getAuthor(),
-            iconSet: this.getIcons(),
-            siteName: this.getSiteName(),
-            thumbnail: this.getThumbnail(),
-            description: this.getDescription(),
-            keywords: this.getKeywords(),
-            readingTime: this.getReadingTime(),
-        };
-    }
+  /**
+    * Gather website info
+    * @return {any} data
+    */
+  getData() {
+    return {
+      url: this.getUrl(),
+      type: this.getType(),
+      logo: this.getLogo(),
+      title: this.getTitle(),
+      author: this.getAuthor(),
+      iconSet: this.getIcons(),
+      siteName: this.getSiteName(),
+      thumbnail: this.getThumbnail(),
+      description: this.getDescription(),
+      keywords: this.getKeywords(),
+      readingTime: this.getReadingTime(),
+    };
+  }
 }
 
 export default ExtractWeb;
--- functions/src/common/helpers.ts
+++ functions/src/common/helpers.ts
@@ -1,31 +1,31 @@
-import { wordsToRemove } from './static';
+import {wordsToRemove} from "./static";
 
 export const extractKeywords = (text: string, limit = 25) => {
-    const words = text.split(/\s+/);
+  const words = text.split(/\s+/);
 
-    const frequencyMap: any = {};
-    for (const word of words) {
-        const cleanedWord = word.toLowerCase().replace(/[^\w\s]/gi, '');
-        if (cleanedWord) {
-            frequencyMap[cleanedWord] = (frequencyMap[cleanedWord] || 0) + 1;
-        }
+  const frequencyMap: any = {};
+  for (const word of words) {
+    const cleanedWord = word.toLowerCase().replace(/[^\w\s]/gi, "");
+    if (cleanedWord) {
+      frequencyMap[cleanedWord] = (frequencyMap[cleanedWord] || 0) + 1;
     }
+  }
 
-    const sortedWords = Object.keys(frequencyMap).sort(
-        (a, b) => frequencyMap[b] - frequencyMap[a]
-    );
+  const sortedWords = Object.keys(frequencyMap).sort(
+    (a, b) => frequencyMap[b] - frequencyMap[a]
+  );
 
-    const filteredWords = sortedWords.filter(
-        (word) => !wordsToRemove.includes(word)
-    );
+  const filteredWords = sortedWords.filter(
+    (word) => !wordsToRemove.includes(word)
+  );
 
-    const numKeywords = limit;
-    return filteredWords.slice(0, numKeywords);
+  const numKeywords = limit;
+  return filteredWords.slice(0, numKeywords);
 };
 
 export const estimateReadingTime = (content: string, wordPerMinute = 150) => {
-    const wordCount = content.trim().split(/\s+/).length;
+  const wordCount = content.trim().split(/\s+/).length;
 
-    const readingTime = Math.ceil(wordCount / wordPerMinute);
-    return readingTime;
+  const readingTime = Math.ceil(wordCount / wordPerMinute);
+  return readingTime;
 };
