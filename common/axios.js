9aae17c05319728f78d929628d517e7155c5ac77
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2023-08-05 23:51:22 +0530

    refactor: APIs

--- common/axios.js
+++ common/axios.js
@@ -1,39 +1,6 @@
 const axios = require('axios');
 
-const APIRequest = (callback) => {
-    return new Promise((resolve, reject) => {
-        return callback
-            .then((response) => resolve(response))
-            .catch((error) => {
-                if (error instanceof axios.AxiosError) {
-                    if (error.response && error.response.status === 403) {
-                        reject({ message: 'Forbidden', statusCode: 403 });
-                    }
-                } else {
-                    reject(error);
-                }
-            });
-    });
-};
-
-exports.getUrlData = (url) => {
-    return new Promise((resolve, reject) => {
-        return axios
-            .get(url, {
-                headers: { 'Accept-Encoding': 'gzip' },
-            })
-            .then((response) => resolve(response))
-            .catch((error) => {
-                if (
-                    error instanceof axios.AxiosError &&
-                    error.response &&
-                    error.response.status === 403
-                ) {
-                    reject({ message: 'Forbidden', statusCode: 403 });
-                } else reject(error);
-            });
-    });
-};
+const { APIRequest } = require('./helpers');
 
 exports.getUrlData = async (url) =>
     await APIRequest(
--- routes/web.route.js
+++ routes/web.route.js
@@ -5,9 +5,7 @@ const WebController = require('../controllers/web.controller');
 const router = express.Router();
 
 router.post('/', WebController.extractWeb);
-
 router.post('/links', WebController.gatherAllLinks);
-
 router.post('/images', WebController.gatherAllImages);
 
 module.exports = router;
